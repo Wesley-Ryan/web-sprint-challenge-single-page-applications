fc86c00aaf582c444640a789070c4b547d6b70d6
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-16 09:43:18 -0700

    All inputs are upating state on PizzaForm

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,83 +1,154 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const initialValues= { 
+    size: '', 
+    sauce: '',
+    pepperoni:false, 
+    cheese: false, 
+    meatLovers: false,
+    veggieLovers: false,
+    instructions:""
+}
+
+const order = false
 
 const PizzaForm = () => {
+    const [formValues,setFormValues]=useState(initialValues)
+    const [pizza, setPizza]=useState("Your order is on the way")
+
+
+    const onChange = (event) => {
+        const { name, value, type, checked } = event.target
+        const valueToUse = type === 'checkbox' ? checked : value
+        setFormValues({
+            ...formValues,
+            [name]: valueToUse
+          })
+      }
+
+
     return (
         <div >
             <h2>Build Your Own Pizza</h2>
+
+            {order && <div>{pizza}</div>}
+
             <form className='form-container'>
                 <label>
                     <h3>Choose Your Size</h3>
                 </label>
                 <div id='drop-down'>
                     <select 
-                        name='role'
+                        name='size'
+                        value={formValues.size}
+                        onChange={onChange}
                         >
                         <option value=''>- Choose your size -</option>
-                        <option value='small'>Small</option>
-                        <option value='medium'>Medium</option>
-                        <option value='large'>Large</option>
-                        <option value='x-large'>Extra Large</option>
+                        <option value='Small' 
+                        
+                        >
+                            Small
+                        </option>
+                        <option value='Medium'
+                       
+                        >
+                            Medium
+                        </option>
+                        <option value='Large'
+                        
+                        >
+                            Large
+                        </option>
+                        <option value='X-Large'
+
+                        >
+                            Extra Large
+                        </option>
                     </select>
                 </div>
 
                 <label id='bottom-border'>
-                <h3>Choose Your Sauce</h3>
+                    <h3>Choose Your Sauce</h3>
                 </label>
                 <div className='choices'>
                     <div>
-                        <input type='radio' /> House Red
+                        <input type='radio' 
+                        name='sauce'
+                        value='House Red'
+                        checked={formValues.sauce === 'House Red'}
+                        onChange={onChange}
+                        /> House Red
                     </div>
                     <div>
-                        <input type='radio' /> Garlic Ranch
+                        <input type='radio'
+                        name='sauce'
+                        value="Garlic Ranch"
+                        checked={formValues.sauce === 'Garlic Ranch'}
+                        onChange={onChange} 
+                        /> Garlic Ranch
                     </div>
                     <div>
-                        <input type='radio' /> Kentucky BBQ
+                        <input type='radio'
+                        name='sauce'
+                        value="Kentucky BBQ"
+                        checked={formValues.sauce === 'Kentucky BBQ'}
+                        onChange={onChange} 
+                        /> Kentucky BBQ
                     </div>
                     <div>
-                        <input type='radio' /> Spinach Alfredo
+                        <input type='radio'
+                        name='sauce'
+                        value="Spinach Alfredo"
+                        checked={formValues.sauce === 'Spinach Alfredo'}
+                        onChange={onChange} 
+                        /> Spinach Alfredo
                     </div>
                 </div>
 
                 <label id='bottom-border'>
-                <h3>Add Toppings</h3>
+                    <h3>Add Toppings</h3>
                 </label>
         {/* ////////// CHECKBOXES ////////// */}
         {/* ////////// CHECKBOXES ////////// */}
         {/* ////////// CHECKBOXES ////////// */}
         <div className='toppings'>
-        <div className='section'>
-        <div>Cheese
+            <div className='section'>
+            <div>Cheese
             <input 
             type='checkbox'
-            name='pepperoni'
+            name="cheese"
+            checked={formValues.cheese}
+            onChange={onChange}
             />
 
         </div>
-
-        
         <div>Pepperoni
             <input 
             type='checkbox'
-            name='pepperoni'
+            name="pepperoni"
+            checked={formValues.pepperoni}
+            onChange={onChange}
             />
-
-        </div>
+            </div>
         </div>
         <div className='section'>
-        <div>Meat Lovers
+            <div>Meat Lovers
             <input 
             type='checkbox'
             name='meatLovers'
+            checked={formValues.meatLovers}
+            onChange={onChange}
             />
 
-        </div>
-        <div >
-        Veggie Lovers
+            </div>
+            <div >Veggie Lovers
             <input 
             type='checkbox'
             name='veggieLovers'
+            checked={formValues.veggieLovers}
+            onChange={onChange}
             />
-        </div>
+            </div>
         </div>
         </div>
             <label id='bottom-border'>
@@ -85,6 +156,9 @@ const PizzaForm = () => {
                 </label>
                 <textarea id='instructions'
                 type='text'
+                name='instructions'
+                value={formValues.instructions}
+                onChange={onChange}
                 />
 
                 <button id='submit'>Make My PIZZA</button>
