02f7c06ad4f0f430eeed878a7395d63da671ca2c
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-16 08:15:43 -0700

    Added React router and built route to PizzaForm component

---
+++ src/components/PizzaForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PizzaForm = () => {
+    return (
+        <div className='form-container'>
+            <h2>Order Pizza</h2>
+        </div>
+     );
+}
+ 
+export default PizzaForm;
